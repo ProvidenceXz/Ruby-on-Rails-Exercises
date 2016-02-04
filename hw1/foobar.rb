229918ebff6bf2d4f14b21ef41c973364f33a385
Provi Zhang <zhangxian1942@live.com>
2016-02-04 01:16:47 +0000

    Finished homework 1

--- hw1/foobar.rb
+++ hw1/foobar.rb
@@ -1,3 +1,7 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(arr)
+    arr.each_index { |i| arr[i] = arr[i].to_i + 2 }
+    arr.select { |n| n.even? && n <= 10 }.uniq.inject(0, :+)
+  end
 end
--- hw1/hw1.rb
+++ hw1/hw1.rb
@@ -1,13 +1,22 @@
+# takes two integers, a and b, and calculates the sum of a and b, 
+# hen returns the square of the sum.
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a + b) ** 2
 end
 
+# takes in an array of integers, sorts it, then increments every 
+# element by 1, and returns it
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  return a.each_index { |i| a[i] += 1}.sort
 end
 
+# takes in two strings, a first_name and last_name. It will return 
+# the first_name concatenated with the last_name, along with a space in between.
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
